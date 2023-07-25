046cea904953bec813b3a155d4742091e945449c
vinay2812 <vinaysarda2812@gmail.com>
2023-07-26 01:33:02 +0530

    ref: restructure middlewares

--- src/connections/server.ts
+++ src/connections/server.ts
@@ -1,8 +1,31 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { PORT } from "../constants";
 import logger from "../utils/logger";
 
 export const app = express();
+
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+
+function shouldCompress(req: Request, res: Response) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+  return compression.filter(req, res);
+}
+app.use(cors())
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  compression({
+    level: 9,
+    filter: shouldCompress,
+  })
+);
+app.use(helmet());
+app.use(morgan(":status :method :url :response-time ms"));
 
 export const server = app.listen(PORT, () => {
   logger.info(`🚀 [server]: running on port:${PORT}`);
